ed2bd8845c52f5d1c42916c5f2b9504d453d2658
LedouxK <kevin.ledoux@epitech.eu>
2025-01-10 17:04:16 +0100

    Configuration PaaS avec Azure App Service et MySQL Flexible Server

--- main.tf
+++ main.tf
@@ -11,51 +11,63 @@ provider "azurerm" {
   features {}
 }
 
-variable "docker_image" {}
-variable "docker_registry_url" {}
-variable "docker_registry_username" {}
-variable "docker_registry_password" {}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "rg-laravel-app"
-  location = "West Europe"
-}
-
-resource "azurerm_container_registry" "acr" {
-  name                = "acrlaravel${random_string.unique.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location           = azurerm_resource_group.rg.location
-  sku                = "Basic"
-  admin_enabled      = true
+data "azurerm_resource_group" "devtest" {
+  name = "t-clo-901-rms-0"
 }
 
 resource "azurerm_service_plan" "app_service_plan" {
   name                = "asp-laravel"
-  resource_group_name = azurerm_resource_group.rg.name
-  location           = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.devtest.name
+  location           = data.azurerm_resource_group.devtest.location
   os_type            = "Linux"
   sku_name           = "B1"
 }
 
+resource "azurerm_mysql_flexible_server" "mysql" {
+  name                = "mysql-laravel-app"
+  resource_group_name = data.azurerm_resource_group.devtest.name
+  location           = data.azurerm_resource_group.devtest.location
+  
+  administrator_login    = "laravel"
+  administrator_password = random_password.mysql_password.result
+  
+  sku_name              = "B_Standard_B1s"
+  version               = "8.0.21"
+  
+  zone                  = "1"
+}
+
+resource "azurerm_mysql_flexible_database" "database" {
+  name                = "laravel"
+  resource_group_name = data.azurerm_resource_group.devtest.name
+  server_name         = azurerm_mysql_flexible_server.mysql.name
+  charset             = "utf8mb4"
+  collation          = "utf8mb4_unicode_ci"
+}
+
 resource "azurerm_linux_web_app" "app" {
   name                = "app-laravel-${random_string.unique.result}"
-  resource_group_name = azurerm_resource_group.rg.name
-  location           = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.devtest.name
+  location           = data.azurerm_resource_group.devtest.location
   service_plan_id    = azurerm_service_plan.app_service_plan.id
 
   site_config {
     application_stack {
-      docker_image     = var.docker_image
-      docker_image_tag = "latest"
+      php_version = "8.2"
     }
+    always_on = true
   }
 
   app_settings = {
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-    "DOCKER_REGISTRY_SERVER_URL"          = var.docker_registry_url
-    "DOCKER_REGISTRY_SERVER_USERNAME"     = var.docker_registry_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD"     = var.docker_registry_password
-    "APP_KEY"                            = "${base64encode(random_string.app_key.result)}"
+    "APP_KEY"           = base64encode(random_string.app_key.result)
+    "APP_ENV"           = "production"
+    "APP_DEBUG"         = "false"
+    "DB_CONNECTION"     = "mysql"
+    "DB_HOST"          = azurerm_mysql_flexible_server.mysql.fqdn
+    "DB_PORT"          = "3306"
+    "DB_DATABASE"      = azurerm_mysql_flexible_database.database.name
+    "DB_USERNAME"      = azurerm_mysql_flexible_server.mysql.administrator_login
+    "DB_PASSWORD"      = azurerm_mysql_flexible_server.mysql.administrator_password
   }
 }
 
@@ -67,5 +79,10 @@ resource "random_string" "unique" {
 
 resource "random_string" "app_key" {
   length  = 32
+  special = true
+}
+
+resource "random_password" "mysql_password" {
+  length  = 16
   special = true
 } 
